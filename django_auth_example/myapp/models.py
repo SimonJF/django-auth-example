167a3e9efb6d2411136aefa33a885fea860f2709
Simon Fowler <simon@simonjf.com>
2024-08-14 12:21:50 +0100

    stuff

--- django_auth_example/myapp/models.py
+++ django_auth_example/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+
+def default_user():
+    try:
+        return User.objects.get(name="DefaultUser")
+    except:
+        default_user = User(name = "DefaultUser")
+        default_user.save()
+        return default_user
+
--- django_auth_example/myapp/views.py
+++ django_auth_example/myapp/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Comment, default_user
+from .forms import CommentForm
+
+
+def index(request):
+    comments = Comment.objects.order_by("-id")
+    context = {
+        "comments": comments,
+        "form": CommentForm()
+    }
+    return render(request, "myapp/index.html", context)
+
+def post_comment(request):
+    if request.method != "POST":
+        return HttpResponseRedirect("/myapp")
+
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        data = form.cleaned_data
+        c = Comment(user=default_user(), comment=data["body"])
+        c.save()
+    return HttpResponseRedirect("/myapp")
